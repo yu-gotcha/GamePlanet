ddd9c1599a2439110f3e6e13e599c8af1994cca8
yu-gotcha <yu.gotcha@gmail.com>
2021-06-23 09:48:37 +0900

    revised

---
+++ src/main/java/com/yugotcha/gameplanet/controller/GameController.java
@@ -0,0 +1,9 @@
+package com.yugotcha.gameplanet.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@RequestMapping(value = {"/game1", "/game2", "/game3", "/game4", "/game5", "/game6"})
+public class GameController {
+
+}
